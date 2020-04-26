105b12f8e85f060394b6d1348d788636c53e7160
Keegan Leary <leary.keegan@gmail.com>
2020-04-26 10:13:55 -0700

    first commit

---
+++ src/game_objects.lua
@@ -0,0 +1,49 @@
+--[[
+    GD50
+    Legend of Zelda
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+]]
+
+GAME_OBJECT_DEFS = {
+    ['switch'] = {
+        type = 'switch',
+        texture = 'switches',
+        frame = 2,
+        width = 16,
+        height = 16,
+        solid = false,
+        defaultState = 'unpressed',
+        states = {
+            ['unpressed'] = {
+                frame = 2
+            },
+            ['pressed'] = {
+                frame = 1
+            }
+        }
+    },
+
+    ['heart'] = {
+        type = 'hearts',
+        texture = 'hearts',
+        frame = 5,
+        width = 16,
+        height = 16,
+        solid = false,
+        --consumeable = true,
+        defaultState = 'idle',
+        states = {
+            ['idle'] = {
+                frame = 5
+            },
+        },
+        -- onCollide = function()
+        -- end
+    },
+
+    ['pot'] = {
+        -- TODO
+    }
+}
